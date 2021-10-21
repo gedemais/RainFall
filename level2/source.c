780b6e9d0fb05a5375c21ee5c6ef2799002fed3a
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-10-21 17:01:39 +0200

    source level2 added

---
+++ level2/source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void	p(void)
+{
+	int		prot; // Will be pushed first on the stack, so that we can check the 4 bytes following the buffer.
+	char	buffer[76];
+
+	fflush(stdout);
+	gets(buffer);
+	if ((prot & 0xb0000000) == 0xb0000000)
+	{
+		printf("%p\n", (void*)prot);
+		exit(1);
+	}
+
+	puts(buffer);
+	strdup(buffer);
+}
+
+int		main(void)
+{
+	p();
+	return (0);
+}
