f5199ad0c651ca5c5ce2f75970b0eed2436d5234
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-10-23 19:04:40 +0200

    level6 walkthrough and source done + bonus4/ removed

--- level6/source.c
+++ level6/source.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void	n(void)
+{
+	system("/bin/cat /home/user/level7/.pass");
+}
+
+void	m(void)
+{
+	puts("Nope");
+}
+
 int     main(int argc, char**argv)
 {
+	char	*a;
+	char	*b;
+	void	(*func)(void) = NULL;
+
+	a = malloc(64);
+	b = malloc(4);
+	argv[1] += (void*)4;
+	strcpy(a, argv[1]);
+	(*func) = b;
+	(*func)();
     return (0);
 }
