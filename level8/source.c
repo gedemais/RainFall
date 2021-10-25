bc0b30e5d7732e025824efa307c44e61ae1146ff
morgan guy <morganguy@morgans-MacBook-Pro.local>
2021-10-25 17:04:29 +0200

    en cours

--- level8/source.c
+++ level8/source.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char    *g_auth="auth ";
+char    *g_service="service";
+
 int     main(int argc, char**argv)
 {
+    char    str[128];
+
+    printf("%d, %d", g_auth, g_service);
+    if (fgets(str, 128, stdin) != 0)
+    {
+        if (strncmp(g_auth, str, 5) == 0)
+        {
+            g_auth = malloc(4);
+        }
+    }
     return (0);
 }
