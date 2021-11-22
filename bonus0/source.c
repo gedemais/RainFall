c106db7a3caa70a768c1ee46bfc68e0c0df9366d
morgan guy <morganguy@morgans-MacBook-Pro.local>
2021-11-22 16:10:29 +0100

    bonus0 walkthrough and source to finish and source of level9 too

--- bonus0/source.c
+++ bonus0/source.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char    *p(char *ptr, const char *ptr2)
+{
+    char    buffer[4096];
+
+    puts(ptr2);
+    read(0, buffer, 4096);
+    buffer[strchr(buffer, '\n')] = '\0';
+    return (strncpy(ptr, buffer, 20));
+}
+
+char    *pp(char *buf)
+{
+    
+}
+
 int     main(int argc, char**argv)
 {
+    char    buf[64];
+
+    pp(buf);
+    puts(buf);
     return (0);
 }
--- level9/source.c
+++ level9/source.c
@@ -3,5 +3,8 @@
 
 int     main(int argc, char**argv)
 {
+    if (argc < 1)
+        exit(1);
+
     return (0);
 }
