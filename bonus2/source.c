94470bf57abfb8c07391751c68ac09c949e07066
jbirdqiqat <MbDBt.BN9hp@protonmail.com>
2021-12-01 01:46:20 +0100

    bonus2 done

--- bonus2/source.c
+++ bonus2/source.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	langage;
+
+int	greetuser(char *username)
+{
+	char	str[72];
+
+	if (langage == 0)
+		strcpy(str, "Hello ");
+	else if (langage == 1)
+		strcpy(str, "Hyvää päivää ");
+	else if (langage == 2)
+		strcpy(str, "Goedemiddag! ");
+
+	strcat(str, username);
+	return (puts(str));
+}
+
 int     main(int argc, char**argv)
 {
-    return (0);
+	char		*lang;
+	char		buff[76];
+	char		buff2[80];
+
+	if (argc != 3)
+		return (1);
+
+	memset(buff1, 0, 19);
+	strncpy(buff1, argv[1], 40);
+	strncpy(buff1 + 40, argv[2], 32);
+
+	if (!(lang = getenv("LANG")))
+		return (1);
+
+	langage = 0;
+	if (memcmp(lang, "fi", 2) == 0)
+		langage = 1;
+	if (memcmp(lang, "nl", 2) == 0)
+		langage = 2;
+
+	memcpy(buff2, buff, 19);
+
+    return (greetuser(buff2));
 }
